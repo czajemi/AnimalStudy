84830164ffee98770058d040313c400c75ad46fc
czajemi <e.czajaaa@gmail.com>
2022-06-23 19:03:13 +0200

    database in php, connection to pdo

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+const USERNAME = 'pmqxucxzdhhjvm';
+const PASSWORD = '244c94767e973ebb33b563f6b4f1247c99c0fa4fcfb639ae9358c8949df0986d';
+const HOST = 'ec2-52-49-120-150.eu-west-1.compute.amazonaws.com';
+const DATABASE = 'dcn799mj9n451i';
--- controllers/SecurityController.php
+++ controllers/SecurityController.php
@@ -2,24 +2,34 @@
 
 require_once 'AppController.php';
 require_once __DIR__ .'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController {
 
     public function login()
     {
-        $user = new User('emilia@czaja.com', password_hash('admin', PASSWORD_BCRYPT), 'Emilia', 'Czaja');
+        $userRepository = new UserRepository();
+
+        if (!$this->isPost()) {
+            return $this->render('login');
+        }
 
         $email = $_POST['email'];
         $password = $_POST['password'];
 
-        if ($user->getEmail() !== $email){
-            return $this->render( 'login', ['messages' => ['User with this email not exist']]);
-        }
-        if (!password_verify($password, $user->getPassword())){
-            return $this->render( 'login', ['messages' => ['Wrong password']]);
+        $user = $userRepository->getUser($email);
+
+        if (!$user) {
+            return $this->render('login', ['messages' => ['User not found!']]);
         }
 
-        $_SESSION['loggedUser'] = $email;
+        if ($user->getEmail() !== $email) {
+            return $this->render('login', ['messages' => ['User with this email not exist!']]);
+        }
+
+        if ($user->getPassword() !== $password) {
+            return $this->render('login', ['messages' => ['Wrong password!']]);
+        }
 
         $url = "http://$_SERVER[HTTP_HOST]";
         header("Location: {$url}/description");
